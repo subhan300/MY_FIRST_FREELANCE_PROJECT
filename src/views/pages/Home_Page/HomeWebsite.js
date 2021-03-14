3adc5b0ee518603d64e8c938411aad6db5f78291
subhan300 <64373402+subhan300@users.noreply.github.com>
2021-03-15 01:00:10 +0500

    Update HomeWebsite.js

--- src/views/pages/Home_Page/HomeWebsite.js
+++ src/views/pages/Home_Page/HomeWebsite.js
@@ -4,7 +4,7 @@ function HomeWebsite() {
     return (
         <div>
            <h1>MY HOME PAGE</h1> 
-           <h2>DESIGN IT j wjkasjkjk</h2>
+           <h2>NOW CHECK IT AGAIN sj</h2>
         </div>
     )
 }
